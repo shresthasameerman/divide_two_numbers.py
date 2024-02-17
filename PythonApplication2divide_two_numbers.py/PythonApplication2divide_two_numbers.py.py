d8ecc2be7a2a90bd3db9689a06c0a55be6beea0f
sameerman <shresthasamerman@gmail.com>
2024-02-17 19:42:15 +0545

    Add project files.

---
+++ PythonApplication2divide_two_numbers.py/PythonApplication2divide_two_numbers.py.py
@@ -0,0 +1,7 @@
+def divide_two_numbers():
+    num1=int(input("enter the 1st number:"))
+    num2=int(input("enter the 2nd number:"))
+    result=num1/num2
+    print(f"the result of {num1}divided by {num2} to two decimal place is {result:.2f}")
+divide_two_numbers()
+
